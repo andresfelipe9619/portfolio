9428c5c8ae141258c6be3fe15d72e0b29bf8b4d8
Andrés Suárez | @suarezdev96 <andresfelipe9619@gmail.com>
2025-08-28 09:19:30 -0500

    📈 Integrate Google Analytics: add `react-ga4` to track analytics and initialize with tracking ID.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-1K72061LE9');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
