810717be2caecc7239b165f073d81978e669f72b
Andrés Suárez | @suarezdev96 <andresfelipe9619@gmail.com>
2025-09-01 17:31:47 -0500

    ✨ Integrate Vercel Analytics: add `@vercel/analytics` package and `<Analytics />` component for improved tracking and insights.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { initGA } from './lib/ga';
 import * as Sentry from '@sentry/react';
+import { Analytics } from '@vercel/analytics/react';
 
 Sentry.init({
   dsn: 'https://869f2b57b98cbe1f86a16043a6f3fd51@o267366.ingest.us.sentry.io/4509940389707776',
@@ -22,6 +23,7 @@ initGA();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <App />
     </BrowserRouter>
